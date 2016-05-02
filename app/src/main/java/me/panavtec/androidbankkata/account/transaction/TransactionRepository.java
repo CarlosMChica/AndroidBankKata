6b0bf42d59285675f66130ed39851e0ffa11bfa7
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2016-05-02 16:22:20 +0100

    Inner loop - Green: Implement repository store transactions

--- app/src/main/java/me/panavtec/androidbankkata/account/transaction/TransactionRepository.java
+++ app/src/main/java/me/panavtec/androidbankkata/account/transaction/TransactionRepository.java
@@ -1,12 +1,18 @@
 package me.panavtec.androidbankkata.account.transaction;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TransactionRepository {
+
+  private final ArrayList<Transaction> transactions = new ArrayList<>();
+
   public void store(Transaction transaction) {
+    transactions.add(transaction);
   }
 
   public List<Transaction> transactions() {
-    return null;
+    return Collections.unmodifiableList(transactions);
   }
 }
