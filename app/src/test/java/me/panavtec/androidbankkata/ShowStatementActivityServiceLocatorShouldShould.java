7fb17baff0f7ad1400560cb841dfdcdf78e66177
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2016-05-02 15:27:29 +0100

    Inner loop - Red: Test account provisioning

---
+++ app/src/test/java/me/panavtec/androidbankkata/ShowStatementActivityServiceLocatorShouldShould.java
@@ -0,0 +1,18 @@
+package me.panavtec.androidbankkata;
+
+import me.panavtec.androidbankkata.account.BankAccount;
+import org.junit.Test;
+
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class ShowStatementActivityServiceLocatorShouldShould {
+
+  @Test public void provide_an_account_with_dependencies() {
+    ShowStatementActivityServiceLocator serviceLocator = new ShowStatementActivityServiceLocator();
+
+    BankAccount account = serviceLocator.getAccount();
+
+    assertThat(account, notNullValue());
+  }
+}
