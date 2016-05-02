abc15e855edd891a7d6e1c98e1a2849622898173
Carlos Morera de la Chica <carlosdelachica@gmail.com>
2016-05-02 14:54:51 +0100

    Inner loop - Green: Implement activity view

--- app/src/main/java/me/panavtec/androidbankkata/ShowStatementActivity.java
+++ app/src/main/java/me/panavtec/androidbankkata/ShowStatementActivity.java
@@ -1,8 +1,21 @@
 package me.panavtec.androidbankkata;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import me.panavtec.androidbankkata.account.statement.StatementView;
 
 public class ShowStatementActivity extends AppCompatActivity implements StatementView {
 
+  private RecyclerView recyclerView;
+
+  @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_show_statement);
+
+    recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+  }
 }
